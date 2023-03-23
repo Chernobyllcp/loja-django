9ae1703bfa38745ec7d9cae8090aedcc728f2812
Chernobyllcp <brenodr.cp@hotmail.com>
2023-03-23 14:59:30 -0300

    site

--- projectloja/lojauth/urls.py
+++ projectloja/lojauth/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from lojauth import views
 
 urlpatterns = [
-    path('cadastrar/', views.cadastrar,name='cadastrar')
+    path('cadastrar/', views.cadastrar,name='cadastrar'),
+    path('login/', views.handlelogin,name='handlelogin'),
     
 ]
--- projectloja/lojauth/views.py
+++ projectloja/lojauth/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+
+
+
+
 
 def cadastrar(request):
+    if request.method=="POST":
+        email = request.POST['email']
+        password = request.POST['pass1']
+        confirme_password = request.POST['pass2']
+        if password != confirme_password:
+            messages.warning(request,"SENHA E CONFIRMAÇÃO NÃO CORRESPONDEM!")
+            return render(request,'auth/cadastrar.html')
+        
+        
+        try:
+            if User.objects.get(usarname=email):
+                messages.warning(request,"Este email já é cadastrado")
+                return render(request,'auth/cadastrar.html')
+        except Exception as identifier:
+            pass
+        
+        myuser = User.objects.create_superuser(email, email, password)
+        myuser.save()
+        messages.info(request,"Cadastro realizado com sucesso. Faça seu login!!")
+        return redirect('/lojauth/login')
     return render(request,'auth/cadastrar.html' )
+
+
+
+def handlelogin(request):
+    return render(request,'auth/login.html' )
